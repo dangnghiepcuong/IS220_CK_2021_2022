d570f7d5c0006dcdfada987ab2a0da092c7daca9
Đặng Nghiệp Cường <20520418@gm.uit.edu.vn>
2023-02-08 21:29:54 +0700

    das

--- Controllers/PlaylistController.cs
+++ Controllers/PlaylistController.cs
@@ -12,6 +12,7 @@ namespace IS220_CK_2021_2022.Controllers
     {
         public IActionResult Index()
         {
+            NguoiNghe();
             return View();
         }
 
@@ -28,18 +29,20 @@ namespace IS220_CK_2021_2022.Controllers
                 NguoiNghe nn = new NguoiNghe();
                 nn.MaNN = reader["MaNN"] as string;
                 nn.TenNN = reader["TenNN"] as string;
+                nn.GioiTinh = "";
                 lnn.Add(nn);
             }
+            
             ViewBag.lnn = lnn;
             return View("Index");
         }
 
-        public IActionResult BaiHat(string nn)
+        public IActionResult Playlist(string MaNN)
         {
             SqlConnection conn = new SqlConnection("Data source=localhost; Initial Catalog=Contacts; Integrated Security=True; Database=IS220_CK_2021_2022");
             conn.Open();
-            SqlCommand cmd = new SqlCommand("select * from PLAYLIST where MaNN = @nn");
-            cmd.Parameters.AddWithValue("nn", nn);
+            SqlCommand cmd = new SqlCommand("select * from PLAYLIST where MaNN = @nn", conn);
+            cmd.Parameters.AddWithValue("nn", MaNN);
             var reader = cmd.ExecuteReader();
             List<Playlist> pl = new List<Playlist>();
             while (reader.Read())
@@ -51,6 +54,30 @@ namespace IS220_CK_2021_2022.Controllers
             }
             ViewBag.pl = pl;
             return View("Index");
+        }
+
+        public IActionResult BaiHat(string pl)
+        {
+            SqlConnection conn = new SqlConnection("Data source=localhost; Initial Catalog=Contacts; Integrated Security=True; Database=IS220_CK_2021_2022");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand(
+                "select B.TenBaiHat, C.TenCaSi " +
+                "from PLAYLIST_BAIHAT PB, BAIHAT B, CASI_BAIHAT CB, CASI C " +
+                "where MaPlayList = @ma and PB.MaBaiHat = B.MaBaiHat " +
+                "and B.MaBaiHat = CB.MaBaiHat and CB.MaCaSi = C.MaCaSi"
+                , conn);
+            cmd.Parameters.AddWithValue("ma", pl);
+            var reader = cmd.ExecuteReader();
+            List<BaiHat> lbh = new List<BaiHat>();
+            while (reader.Read())
+            {
+                BaiHat bh = new BaiHat();
+                bh.TenBaiHat = reader["TenBaiHat"] as string;
+                bh.MaBaiHat = reader["TenCaSi"] as string;
+                lbh.Add(bh);
+            }
+            ViewBag.BC = lbh;
+            return View("BaiHat");
         }
 
         public IActionResult Xoa(string pl)
